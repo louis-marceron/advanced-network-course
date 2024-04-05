eca6f3c48b2d0cbf70465898a214499b77982f08
Louis Marceron <louis.marceron@protonmail.com>
2024-04-05 16:42:30 +0200

    add 71/72 windows pc ip configurations

---
+++ lab4/pc.bat
@@ -0,0 +1,3 @@
+netsh interface ipv4 set address "Cisco" static 192.168.7.71 255.255.255.0 192.168.7.0
+
+netsh interface ipv4 set address "Cisco" static 192.168.7.72 255.255.255.0 192.168.7.0
--- lab5/pc.bat
+++ lab5/pc.bat
@@ -1,3 +1,8 @@
 netsh interface ipv4 set address "Cisco" static 172.16.3.31 255.255.255.0 172.16.3.30
 
 netsh interface ipv4 set address "Cisco" static 172.16.3.32 255.255.255.0 172.16.3.30
+
+
+netsh interface ipv4 set address "Cisco" static 172.16.3.71 255.255.255.0 172.16.3.70
+
+netsh interface ipv4 set address "Cisco" static 172.16.3.72 255.255.255.0 172.16.3.70
