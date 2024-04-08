76cb803a4cafbedb18f3e93321869da092a336df
Louis Marceron <louis.marceron@protonmail.com>
2024-04-08 12:21:14 +0200

    router configs + pc configs

--- lab5/pc.bat
+++ lab5/pc.bat
@@ -10,7 +10,16 @@ netsh interface ipv4 set address "Cisco" static 172.16.3.31 255.255.255.0 172.16
 
 netsh interface ipv4 set address "Cisco" static 172.16.3.32 255.255.255.0 172.16.3.30
 
+# PC_71
+# 172.16.7.71/24	172.16.7.70
+# 2001:db8:7::71/64	2001:db8:7::70
 
 netsh interface ipv4 set address "Cisco" static 172.16.7.71 255.255.255.0 172.16.7.70
+netsh interface ipv6 set address "Cisco" static 2001:db8:7::71/64 2001:db8:7::70
+
+# PC_72
+# 172.16.7.72/24	172.16.7.70
+# 2001:db8:7::72/64	2001:db8:7::70
 
 netsh interface ipv4 set address "Cisco" static 172.16.7.72 255.255.255.0 172.16.7.70
+netsh interface ipv6 set address "Cisco" static 2001:db8:7::72/64 2001:db8:7::70
