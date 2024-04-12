99e89cd96d4c9b815b99c7db2500deb5bd65fed0
Louis Marceron <louis.marceron@protonmail.com>
2024-04-12 21:17:07 +0200

    task 1 group 7

---
+++ lab6/pc.bat
@@ -0,0 +1,9 @@
+# PC_71
+# 172.16.10.71		172.16.10.70
+
+netsh interface ipv4 set address "Cisco" static 172.16.10.71 172.16.10.70
+
+# PC_72
+# 172.16.20.72		172.16.20.70
+
+netsh interface ipv4 set address "Cisco" static 172.16.20.72 172.16.20.70
